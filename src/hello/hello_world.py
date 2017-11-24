97f2b17e400e867c0120f2eaf27a96b9ff59f525
mamunur-r <81mamunur@gmail.com>
2017-11-24 01:54:19 +0000

    initial commit

---
+++ src/hello/hello_world.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+
+class Hello:
+    """
+    just to say hello world
+    """
+    def __init__(self):
+        self.hello_back = 'You said '
+
+    def hello_me(self, msg: str):
+        print('Hello world')
+        return self.hello_back+msg+' to me!'
