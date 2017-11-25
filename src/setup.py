21be7c37980d67fd554f7e31190fe18a53735c50
mamunur-r <81mamunur@gmail.com>
2017-11-25 13:22:58 +0000

    added requirement.txt and setup.py files

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hello',
+    version='0.0.0.1',
+    packages=['hello'],
+    url='https://github.com/mamunur-r/python-hello-world',
+    license='',
+    author='mrashid',
+    author_email='81mamunur@gmail.com',
+    description='CI test',
+    install_requires=[]
+)
