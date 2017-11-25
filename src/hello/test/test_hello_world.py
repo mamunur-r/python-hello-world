fdfcccb9bf36c02d9a1bc6be2992d8f75e700df7
mamunur-r <81mamunur@gmail.com>
2017-11-25 13:52:25 +0000

    added unit test code file

---
+++ src/hello/test/test_hello_world.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+import unittest
+from src.hello.hello_world import Hello
+
+
+class HelloTest(unittest.TestCase):
+    """
+    all unit tests
+    """
+
+    def setUp(self):
+        self.hw = Hello()
+
+    def test_hello_me(self):
+        response = self.hw.hello_me('Hello')
+
+        self.assertEqual(response,'You said Hello to me!')
+
