0b619115feb26492c4a49f4960688883fad7f9d3
mamunur-r <81mamunur@gmail.com>
2017-11-25 14:06:08 +0000

    added sum mentod with uint test

--- src/hello/hello_world.py
+++ src/hello/hello_world.py
@@ -13,3 +13,7 @@ class Hello:
     def hello_me(self, msg: str):
         print('Hello world')
         return self.hello_back+msg+' to me!'
+
+    def sum(self, x: int, y: int):
+        result = x + y
+        return result
